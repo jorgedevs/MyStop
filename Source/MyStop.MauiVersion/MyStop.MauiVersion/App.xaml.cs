4806b1896c961d4a749b9fe210985095d0d3292c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-07-25 22:11:01 -0700

    Reorg

---
+++ Source/MyStop.MauiVersion/MyStop.MauiVersion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStop.MauiVersion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Source/MyStop.MauiVersion/MyStop.MauiVersion/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyStop.MauiVersion
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
