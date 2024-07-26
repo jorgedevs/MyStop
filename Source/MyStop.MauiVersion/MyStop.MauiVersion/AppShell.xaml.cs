a79fb7fc0d742987709727872d424a648b14da05
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-07-26 00:50:34 -0700

    Splashscreen and app icon

--- Source/MyStop.MauiVersion/MyStop.MauiVersion/AppShell.xaml.cs
+++ Source/MyStop.MauiVersion/MyStop.MauiVersion/AppShell.xaml.cs
@@ -1,10 +1,9 @@
-﻿namespace MyStop.MauiVersion
+﻿namespace MyStop.MauiVersion;
+
+public partial class AppShell : Shell
 {
-    public partial class AppShell : Shell
+    public AppShell()
     {
-        public AppShell()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
